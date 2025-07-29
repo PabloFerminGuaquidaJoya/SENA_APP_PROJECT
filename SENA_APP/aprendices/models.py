0d6566b2b66e3451b33b74f5e95e31e1f184ef13
PabloFerminGuaquidaJoya <pabloguaquidalhmail.com>
2025-07-29 12:08:12 -0500

    creacion proyecto

---
+++ SENA_APP/aprendices/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Aprendiz(models.Model):
+    documento_identidad = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    telefono = models.CharField(max_length=200, null=True)
+    correo = models.EmailField(null=True)
+    fecha_nacimiento = models.DateField()
+    ciudad = models.CharField(max_length=200, null=True)
+    programa = models.IntegerField(max_length=200)
+    
+    def __str__(self):
+        return f"{self.nombre} {self.apellido} {self.documento_identidad}"
---
+++ SENA_APP/aprendices/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.template import loader
+
+from .models import Aprendiz
+
+def aprendices(request):
+    lista_aprendices = Aprendiz.objects.all().order_by('nombre', 'apellido')
+    template = loader.get_template('lista_aprendices.html')
+    context = {
+        'lista_aprendices': lista_aprendices,
+        'total_aprendices': lista_aprendices.count(),
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def index(request):
+  template = loader.get_template('index.html')
+  return HttpResponse(template.render())
